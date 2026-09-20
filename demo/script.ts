dc3c211a557c6f541fb5de50469bf0912b6c28a7
Darshannikam9767 <darshannikam64@gmail.com>
2026-09-20 16:40:26 +0530

    implemented class in typescript

--- demo/script.ts
+++ demo/script.ts
@@ -1,181 +1,218 @@
-// // // // const input = document.getElementsByClassName(".input");
-// // // // const btn = document.getElementsByClassName(".btn");
+// // // // // const input = document.getElementsByClassName(".input");
+// // // // // const btn = document.getElementsByClassName(".btn");
 
-// // // // let num : number;
+// // // // // let num : number;
 
-// // // // btn[0].addEventListener("click",()=>{
-// // // //     console.log(input);
-    
-// // // // })
+// // // // // btn[0].addEventListener("click",()=>{
+// // // // //     console.log(input);
 
-// // // // let arr : [number,string] = [1.5, "Darshan"];
+// // // // // })
 
-// // // // console.log(arr);
+// // // // // let arr : [number,string] = [1.5, "Darshan"];
 
-// // // // function abcd () : string {
-// // // //     return "hello";
+// // // // // console.log(arr);
+
+// // // // // function abcd () : string {
+// // // // //     return "hello";
+// // // // // }
+
+// // // // // console.log(abcd().toUpperCase());
+
+
+// // // // // enum direction {
+// // // // //     top = "TOP",
+// // // // //     left = "LEFT",
+// // // // //     right = "RIGHT",
+// // // // //     bottom = "BOTTOM",
+// // // // // }
+
+
+// // // // // console.log(direction.bottom);
+
+// // // // // let variable : string | number | number[];
+
+// // // // // variable = "true";
+// // // // // console.log(variable);
+
+// // // // // variable = 20;
+// // // // // console.log(variable);
+
+// // // // // variable = [1,2,3,4,5];
+// // // // // console.log(variable);
+
+// // // // // let variable : string | number;
+
+// // // // // variable = 12;
+
+// // // // // if(typeof variable === "number"){
+// // // // //     variable.toFixed(2);
+// // // // // }else if(typeof variable === "string"){
+// // // // //     variable.toUpperCase();   //in this way it doesn't work...
+// // // // // };
+
+// // // // // function demo(variable: string | number): string | number{
+// // // // //     if(typeof variable === "number"){
+// // // // //         return variable.toFixed(2);
+// // // // //     }else{
+// // // // //         return variable.toUpperCase();
+// // // // //     }
+// // // // // }
+
+// // // // // console.log(demo("darshan"));
+// // // // // console.log(demo(76.5234651));
+
+
+// // // // // let value : string | number; //union 
+
+
+
+// // // // //Intersection
+// // // // type City = {
+// // // //     cityName : string,
+// // // //     population : number,
 // // // // }
 
-// // // // console.log(abcd().toUpperCase());
-
-
-// // // // enum direction {
-// // // //     top = "TOP",
-// // // //     left = "LEFT",
-// // // //     right = "RIGHT",
-// // // //     bottom = "BOTTOM",
+// // // // type Planet = {
+// // // //     planetName : string,
+// // // //     cities : number,
 // // // // }
 
+// // // // type Universe = City & Planet;
 
-// // // // console.log(direction.bottom);
-
-// // // // let variable : string | number | number[];
-
-// // // // variable = "true";
-// // // // console.log(variable);
-
-// // // // variable = 20;
-// // // // console.log(variable);
-
-// // // // variable = [1,2,3,4,5];
-// // // // console.log(variable);
-
-// // // // let variable : string | number;
-
-// // // // variable = 12;
-
-// // // // if(typeof variable === "number"){
-// // // //     variable.toFixed(2);
-// // // // }else if(typeof variable === "string"){
-// // // //     variable.toUpperCase();   //in this way it doesn't work...
-// // // // };
-
-// // // // function demo(variable: string | number): string | number{
-// // // //     if(typeof variable === "number"){
-// // // //         return variable.toFixed(2);
-// // // //     }else{
-// // // //         return variable.toUpperCase();
-// // // //     }
+// // // // let variable : Universe ;
+// // // // variable= {
+// // // //     cityName:"Shirdi",
+// // // //     population : 5636478,
+// // // //     planetName : "Earth",
+// // // //     cities : 34290
 // // // // }
 
-// // // // console.log(demo("darshan"));
-// // // // console.log(demo(76.5234651));
-
-
-// // // // let value : string | number; //union 
+// // // // console.log(variable);
 
 
 
-// // // //Intersection
-// // // type City = {
-// // //     cityName : string,
-// // //     population : number,
+// // // //premitive type aliases
+
+// // // // type Alphabate = string;
+
+// // // // let studnetName : Alphabate;
+
+// // // // studnetName = "Darshan  Nikam";
+
+
+// // // // console.log(typeof studnetName);
+
+
+
+
+
+
+
+// // // // object type aliases
+
+// // // type Car = {
+// // //     name : string,
+// // //     model : number;
+// // //     brand : string,
 // // // }
 
-// // // type Planet = {
-// // //     planetName : string,
-// // //     cities : number,
+// // // let car : Car;
+
+// // // car = {
+// // //     name:"M4 Competetion",
+// // //     model : 2025,
+// // //     brand : "BMW"
 // // // }
 
-// // // type Universe = City & Planet;
-
-// // // let variable : Universe ;
-// // // variable= {
-// // //     cityName:"Shirdi",
-// // //     population : 5636478,
-// // //     planetName : "Earth",
-// // //     cities : 34290
-// // // }
-
-// // // console.log(variable);
-
-
-
-// // //premitive type aliases
-
-// // // type Alphabate = string;
-
-// // // let studnetName : Alphabate;
-
-// // // studnetName = "Darshan  Nikam";
-
-
-// // // console.log(typeof studnetName);
+// // // console.log(car);
+// // // console.log(typeof car);
 
 
 
 
-
-
-
-// // // object type aliases
-
-// // type Car = {
-// //     name : string,
-// //     model : number;
-// //     brand : string,
+// // interface User {
+// //     name : string;
+// //     username : string;
+// //     age : number;
+// //     email : string,
+// //     password : string
 // // }
 
-// // let car : Car;
+// // const getUser = (user : User) => {
+// //     console.log(user.name);
+// //     console.log(user.username);
+// //     console.log(user.age);
+// //     console.log(user.email);
+// //     console.log(user.password);
 
-// // car = {
-// //     name:"M4 Competetion",
-// //     model : 2025,
-// //     brand : "BMW"
 // // }
 
-// // console.log(car);
-// // console.log(typeof car);
+// // let user : User = {
+// //     name : "Darshan Nikam",
+// //     username : "darshuuu",
+// //     age : 21,
+// //     password : "darshan@123",
+// //     email : "darshan@gmail.com"
+// // }
+
+// // getUser(user)
 
 
-
-
-// interface User {
-//     name : string;
-//     username : string;
-//     age : number;
-//     email : string,
-//     password : string
+// interface Food {
+//      name : string
+//      price : number
 // }
 
-// const getUser = (user : User) => {
-//     console.log(user.name);
-//     console.log(user.username);
-//     console.log(user.age);
-//     console.log(user.email);
-//     console.log(user.password);
-    
+// interface MithaFood extends Food{
+//     isSweet : boolean
 // }
 
-// let user : User = {
-//     name : "Darshan Nikam",
-//     username : "darshuuu",
-//     age : 21,
-//     password : "darshan@123",
-//     email : "darshan@gmail.com"
+// interface KhattaFood extends Food{
+//     isSour : boolean
 // }
 
-// getUser(user)
+
+// function getMithaFood(food : MithaFood){
+//     food.isSweet;
+// }
+
+// function getKhattaFood(food : KhattaFood){
+//     food.isSour;
+// }
 
 
-interface Food {
-     name : string
-     price : number
+
+
+
+
+
+// working with class "OOPs"
+
+class Student {
+    id !: number;
+    name !: string;
+    className !: string;
+
+    setStudent(id: number, name: string, className: string) {
+        this.id = id;
+        this.name = name;
+        this.className = className;
+    }
+
+    getStudent() {
+        console.log(`ID = ${this.id}`);
+        console.log(`NAME = ${this.name}`);
+        console.log(`CLASSNAME = ${this.className}`);
+    }
 }
 
-interface MithaFood extends Food{
-    isSweet : boolean
-}
+let s1 = new Student();
 
-interface KhattaFood extends Food{
-    isSour : boolean
-}
+s1.setStudent(1,"Darshan Nikam","FYMCA");
 
+let s2 = new Student();
 
-function getMithaFood(food : MithaFood){
-    food.isSweet;
-}
+s2.setStudent(2,"Krushna More", "FYMCA");
 
-function getKhattaFood(food : KhattaFood){
-    food.isSour;
-}
+s1.getStudent();
+s2.getStudent();
